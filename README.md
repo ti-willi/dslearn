17bc1b13ad57bbcc6e6fd293c34021a00b59c2ad
Willian Comiotto <willianbcomiotto@gmail.com>
2024-03-02 10:33:21 -0300

    Course entity

---
+++ README.md
@@ -0,0 +1,3 @@
+# DSLearn
+
+>Em andamento
--- src/main/java/com/tiwilli/dslearn/entities/User.java
+++ src/main/java/com/tiwilli/dslearn/entities/User.java
@@ -23,6 +23,7 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+
     public User() {
     }
 
